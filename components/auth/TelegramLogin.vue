acde582a140a01dfdb1a3eacc057a369efee4f2c
ladalin05 <dalin715087@gmail.com>
2026-01-31 13:39:06 +0700

    Update TelegramLogin.vue

--- components/auth/TelegramLogin.vue
+++ components/auth/TelegramLogin.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, onBeforeUnmount } from 'vue'
 import { userAuth } from '~/store/userAuth'
 
 const botUsername = 'pureclean_loyality_bot'
@@ -14,30 +14,39 @@ const router = useRouter()
 onMounted(() => {
   if (!import.meta.client) return
 
-  // MUST be global
-  window['onTelegramAuth'] = async (user) => {
+  // Make callback global
+  window.onTelegramAuth = async (user) => {
+    const username = user.username || user.first_name + user.last_name; // Fallback to first name if username is not available
+    const telegram_id = String(user.id); // Telegram user ID
+    console.log("Telegram user:", user);
+    const profile_picture = user.photo_url || "";
+    console.log("Profile picture URL:", profile_picture);
     try {
-      console.log('Telegram user:', user)
-      await userAuthStore.loginWithTelegram(user)
-      router.push('/')
+      await userAuthStore.login(username, telegram_id, profile_picture);
     } catch (error) {
-      console.error('Telegram login error:', error)
+      console.error("Login error:", error);
       await $swal.fire({
-        title: 'Login failed',
-        text: 'Something went wrong. Please try again.',
+        title: 'Login or Signup failed',
+        text: 'Opps have something wrong. Let contact us!',
         icon: 'error',
+        confirmButtonText: 'OK',
         timer: 2000
-      })
+      });
     }
-  }
+  };
 
   const container = document.getElementById('telegram-login')
-  if (!container || container.children.length > 0) return
+  if (!container) return
 
+  // Remove old script (important for SPA navigation)
+  const oldScript = document.getElementById('telegram-widget')
+  if (oldScript) oldScript.remove()
+
+  // Create new script
   const script = document.createElement('script')
+  script.id = 'telegram-widget' // add ID for easy removal
   script.src = 'https://telegram.org/js/telegram-widget.js?22'
   script.async = true
-
   script.setAttribute('data-telegram-login', botUsername)
   script.setAttribute('data-size', 'large')
   script.setAttribute('data-radius', '20')
@@ -46,5 +55,10 @@ onMounted(() => {
   script.setAttribute('data-onauth', 'onTelegramAuth(user)')
 
   container.appendChild(script)
+})
+
+onBeforeUnmount(() => {
+  // Clean up global callback
+  delete window.onTelegramAuth
 })
 </script>
